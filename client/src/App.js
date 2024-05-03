b553bf33ed0c6061bef4eff6ece566c73779ca67
Andrew Miller <miller222111@gmail.com>
2024-05-03 12:09:56 -0700

    now have 3 k8s folders

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-Title">Fibonacci Calculator Kubernetes!</h1>
+          <h1 className="App-Title">Fibonacci Calculator K8s!</h1>
           <Link to="/">Home</Link>
           <Link to="/otherPage">Other Page</Link>
         </header>
